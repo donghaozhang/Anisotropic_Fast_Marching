0283feb90ae6051b827ee220ecec389b7a7cd1c7
donghaozhang <dzha9516@uni.sydney.edu.au>
2016-07-05 11:45:29 +1000

    I modifty the folder structure to let anisotropic fast marching make more sense

--- compareradiusoof.m
+++ compareradiusoof.m
@@ -1,12 +1,12 @@
-% load('mat\eigvoof.mat');
-load('mat\radius_output.mat');
+load('mat\eigvoof.mat');
+% load('mat\radius_output.mat');
 ptsz = size(tree, 1);
 for i = 1 : ptsz
 	input_x = round(tree(i, 3));
 	input_y = round(tree(i, 4));
 	input_z = round(tree(i, 5));
-	% radius = getradiusoof(I_original, input_x, input_y, input_z, eigvoof);
-	radius = getradiusoofradiusmat(I_original, input_x, input_y, input_z, radius_output);
+	radius = getradiusoof(I_original, input_x, input_y, input_z, eigvoof);
+	% radius = getradiusoofradiusmat(I_original, input_x, input_y, input_z, radius_output);
 	% tree(i, 6) = (radius + tree(i, 6)) / 2;
 	% tree(i, 6) = radius / 3;
 	tree(i, 6) = radius;
--- getradiusoof.m
+++ getradiusoof.m
@@ -4,6 +4,7 @@ function radius = getradiusoof(inputMatrix, input_x, input_y, input_z, eigoofmat
 	input_y = constrain(input_y, 1, N);
 	input_z = constrain(input_z, 1, Z);
 	radius = eigoofmat(input_x, input_y, input_z, 1) + eigoofmat(input_x, input_y, input_z, 2) + eigoofmat(input_x, input_y, input_z, 3);
+	% radius = eigoofmat(input_x, input_y, input_z, 1);
 	radius = round(radius);
 	if radius < 0
 		radius = 1;
